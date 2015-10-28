8cb0d73d383180fa77644090c77b1aea58497232
Matt Richardson <matt.richardson@asos.com>
2015-10-28 16:14:27 +0000

    Check whether drive is connected before entering locked section

--- src/ProGetAzureFileShareExtension/FileShareMapper.cs
+++ src/ProGetAzureFileShareExtension/FileShareMapper.cs
@@ -11,38 +11,42 @@ namespace ProGetAzureFileShareExtension
     /// </summary>
     class FileShareMapper : IFileShareMapper
     {
-        private static object lockObject = new object();
+        private static readonly object LockObject = new object();
 
         public void Mount(string driveLetter, string uncPath, string userName, string accessKey, ILog logger)
         {
-            lock(lockObject)
-            {
-                var mountedShares = FilesMappedDrive.GetMountedShares();
+            if (IsShareConnected(uncPath, logger))
+                return;
 
-                foreach (var share in mountedShares)
-                {
-                    if ((share.Path.ToLower() == uncPath.ToLower()) && (share.DriveLetter.ToLower().TrimEnd('\\') == driveLetter.ToLower()))
-                    {
-                        try
-                        {
-                            if (Directory.EnumerateDirectories(uncPath).Any() || Directory.EnumerateFiles(uncPath).Any())
-                            {
-                                logger.DebugFormat("Already connected to '{0}' as drive '{1}'. Skipping re-mount.", share.Path, share.DriveLetter);
-                                return;
-                            }
-                            logger.WarnFormat("Searched for files and directories on the share '{0}', which was mapped as drive '{1}', but didn't find any. " +
-                                              "This shouldn't really happen, as there should be something on the share already." +
-                                              "Assuming that the share is connected as we were able to query for files successfully; skipping re-mount.", share.Path, share.DriveLetter);
-                            return;
-                        }
-                        catch (Exception ex)
-                        {
-                            logger.Debug("Ignorable exception while attempting to enumerate files on existing share. Will attempt to remount.", ex);
-                        }
-                    }
-                }
+            lock (LockObject)
+            {
+                if (IsShareConnected(uncPath, logger))
+                    return;
 
                 FilesMappedDrive.Mount(driveLetter, uncPath, userName, accessKey);
+            }
+        }
+
+        private static bool IsShareConnected(string uncPath, ILog logger)
+        {
+            try
+            {
+                if (Directory.EnumerateDirectories(uncPath).Any() || Directory.EnumerateFiles(uncPath).Any())
+                {
+                    logger.DebugFormat("Already connected to '{0}'. Skipping re-mount.", uncPath);
+                    return true;
+                }
+                logger.WarnFormat(
+                    "Searched for files and directories on the share '{0}', but didn't find any. " +
+                    "This shouldn't really happen, as there should be something on the share already." +
+                    "Assuming that the share is connected as we were able to query for files successfully; skipping re-mount.",
+                    uncPath);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logger.Debug("Ignorable exception while attempting to enumerate files on existing share. Will attempt to remount.", ex);
+                return false;
             }
         }
     }
